3414e92cb0f2f4e179c074139967bdcac03c564a
Madi Atkins <madiatk@umich.edu>
2021-09-12 17:02:50 -0400

    finished generate_tags_list.py (pulls, sorts, and cleans tags and updates database; cleaned up twitter_bot_hannibal.py; moved extract_metadata.py to main branch

--- generate_tags_list.py
+++ generate_tags_list.py
@@ -1,24 +1,25 @@
-import itertools
-import collections
 import json
 import re
 import sqlite3
-import tweepy
-import twitter_info_hannibal
-import datetime
+import pprint
+from pathlib import Path
 
-CACHE_FNAME = "hannibal_fic_info2.json"
+# STEP 0: Enter the name of the JSON file you converted from the fic metadata (extract_metadata.py) CSV. Also, for testing, added PrettyPrint variable, if needed.
+CACHE_FNAME = ""
+pp = pprint.PrettyPrinter()
 
-# Put the rest of your caching setup here:
+# STEP 1: Setting up cache to open file into an easy-to-use variable.
 try:
     cache_file = open(CACHE_FNAME, 'r', encoding="cp1252")
     cache_contents = cache_file.read()
     cache_file.close()
     CACHE_DICTION = json.loads(cache_contents)
+    print("cache is open")
 except:
-	print("cache not open")
+    print("cache not open")
 
-# Write the initial information I want in the database. I won't close the database until I've completed everything I need to do
+
+# STEP 2: Write the initial information I want into the database. I won't close the database until I've completed everything I need to do, but it is important to .commit() after you make changes. For sqlite.connect(), the file name is what you want the data to be called.
 conn = sqlite3.connect("hannibal_fics.db")
 cur = conn.cursor()
 
@@ -28,75 +29,142 @@ cur.execute("CREATE TABLE IF NOT EXISTS HannibalFics (work_id INTEGER PRIMARY KE
 
 # For each dictionary in the list CACHE_DICTION, insert the relevant information into the database
 for fic in CACHE_DICTION:
-	cur.execute("INSERT INTO HannibalFics (work_id, title, relationship, additional_tags, link) VALUES (?, ?, ?, ?, ?)", (fic['work_id'], fic['title'], fic['relationship'], fic['additional tags'], 'https://archiveofourown.org/works/' + str(fic['work_id'])))
+    cur.execute("INSERT INTO HannibalFics (work_id, title, relationship, additional_tags, link) VALUES (?, ?, ?, ?, ?)",
+                (fic['work_id'], fic['title'], fic['relationship'], fic['additional tags'], 'https://archiveofourown.org/works/' + str(fic['work_id'])))
 conn.commit()
-# conn.close()
 print("done with database!")
 
-# Define functions needed to sort and clean the tags
+
+# Define functions needed to sort and clean the tags.
+
+# Grab the word ID and tags for the fics from the database. Returns a list of tuples.
+def grabfromdb():
+    cur.execute("SELECT work_id, additional_tags FROM HannibalFics")
+    grab_tags = cur.fetchall()
+    print("pulled info from database")
+    return grab_tags
 
 
-def grabfromdict():
-	empty_str = ""
-	cur.execute("SELECT work_id, additional_tags FROM HannibalFics WHERE additional_tags != empty_str")
-	grab_tags = cur.fetchall()
-	print("pulled info from database")
-	return grab_tags
+# Create a dictionary from the list of tuples grabfromdb() returns to keep the word IDs associated with the tags.
+def createdict_fromtuples(list_of_tuples):
+    tuple_dict = {}
+    for tup in list_of_tuples:
+        key = tup[0]
+        value = tup[1]
+        tuple_dict[key] = value
+    print("created tuple dictionary")
+    return tuple_dict
 
 
-# takes string and splits it into a list of separated strings based on ', '
-def string_split(tag_string):
-	splitup = re.split(', ', tag_string)
-	return splitup
+# Use the two functions you just defined and assign the output to a variable.
+tag_dict = createdict_fromtuples(grabfromdb())
 
 
-# takes list of strings and dedupes it
-def case_insensitive(tag_list):
-	result = []
-	marker = set()
-
-	for sensitive_tag in tag_list:
-		tag_lower = sensitive_tag.lower()
-		# test presence
-		if tag_lower not in marker:
-			marker.add(tag_lower)
-			# preserve order
-			result.append(sensitive_tag)
-	print("done deduping")
-	return result
+# Create a separated list of tags for each key-value pair in the tag dictionary and reassigns that list of strings as the value. Separates based on ', '
+def string_split(tagDict):
+    for key, value in tagDict.items():
+        splitup = re.split(', ', value)
+        tagDict[key] = splitup
+    print("strings are split")
+    return tagDict
 
 
-def grabfromdict():
-	empty_str = ""
-	cur.execute("SELECT work_id, additional_tags FROM HannibalFics WHERE additional_tags != empty_str")
-	grab_tags = cur.fetchall()
-	print("pulled info from database")
-	return grab_tags
+# Use the string_split() function on tag_dict and assign the output to a variable.
+split = string_split(tag_dict)
 
 
-def write_list_to_file(big_list_o_tags):
-	with open('manual_look_tags.txt', 'w') as file:
-		for tag in big_list_o_tags:
-			file.write(tag + "\n")
-	file.close()
-	print("bad tags file is written")
-	pass
+# Dedupes a list of strings from the tag dictionary. Returns a tuple = (original tagDict, list of deduped tags)
+def case_insensitive(tagDict):
+    result = []
+    marker = set()
+    for key, value in tagDict.items():
+        for tag in value:
+            tag_lower = tag.lower()
+            # test presence
+            if tag_lower not in marker:
+                marker.add(tag_lower)
+                # preserve order
+                result.append(tag)
+    print("done deduping")
+    return tagDict, result
+    # returns tuple of dictionary, list
 
 
-def read_file_make_list():
-	with open('manual_look_tags.txt', 'r') as file:
-		lines = file.read()
-		bad_tags = lines.split('\n')
-		return bad_tags
+# Use the case_insensitive() function on split and assign the output to a variable.
+deduped = case_insensitive(split)
 
 
-# takes two lists: list of tags from row and pre-defined notList
-def unique_tags(myList, notList):
-	unique_list = []
-	for x in myList:
-		if x not in notList:
-			unique_list.append(x)
-		else:
-			pass
-	return unique_list
-		
+# Writes the list of deduped tags to a .txt file for manual review.
+# 'fname' is the string name for your file (remember to include .txt)
+def write_list_to_file(deduped_tuple, fname):
+    with open(fname, 'w') as file:
+        big_list_o_tags = deduped_tuple[1]
+        for tag in big_list_o_tags:
+            file.write(tag + "\n")
+    file.close()
+    print("bad tags file is written")
+    pass
+
+
+# Checks if the manual review tag file exists. If it does, then it will not rerun write_list_to_file() function (which would undo any manual review you've done whenever you run this code). If it doesn't, then it will call the write_list_to_file() function which will write the tags to a file.
+def does_file_exist(deduped_tuple, fname_full):
+    path_to_file = fname_full
+    path = Path(path_to_file)
+    if path.is_file() is True:
+        print("file already exists")
+    if path.is_file() is False:
+        print("file does not exist; writing file")
+        write_list_to_file(deduped_tuple, path_to_file)
+    pass
+
+
+does_file_exist(deduped, "hannibal_ignoreList.txt")
+
+
+# After the tags are written to the .txt file, you will need to manually go through and edit the file and remove any tags that shouldn't be ignored. It should *hopefully* not be as many since it was deduped.
+
+# Creates a list of the manually updated tags from the file to which you wrote.
+# 'fname' is the string name for your file (remember to include .txt)
+def read_file_make_list(fname):
+    with open(fname, 'r') as file:
+        lines = file.read()
+        bad_tags = lines.split('\n')
+        return bad_tags
+
+
+# Use the read_file_make_list() function and assign the output to a variable.
+ignoreList = read_file_make_list("hannibal_ignoreList.txt")
+
+
+# Creates a dictionary of unique tags assigned to their word ID from the tag dictionary created by string_split() compared against the ignoreList list of basic tags not to tweet.
+def unique_tags(tagDict, notList):
+    # reminder: key is an int and value is a list of strings
+    for key, value in tagDict.items():
+        unique_list = []
+        for string in value:
+            if string not in notList:
+                unique_list.append(string)
+            else:
+                pass
+        tagDict[key] = unique_list
+    return tagDict
+
+
+# Use the unique_tags() function and assign the output to a variable.
+final_tag_dict = unique_tags(split, ignoreList)
+
+
+# Use sqlite3 to update the existing database with the new list of updated tags to each row based on their work ID.
+update_query = 'UPDATE HannibalFics SET additional_tags = ? WHERE work_id = ?'
+
+sql_list = []
+sql_list.append(final_tag_dict)
+
+# sqlite3 is finicky so make sure to add str() and int() to the list of tags and work IDs respectively. Important to note that the additional_tags column is no longer a string of words, but a string of a list of strings.
+for item1, item2 in sql_list[0].items():
+    newtags = str(item2)
+    ficid = int(item1)
+    cur.execute(update_query, (newtags, ficid))
+conn.commit()
+print("updated database; closing database")
+conn.close()
--- twitter_bot_hannibal.py
+++ twitter_bot_hannibal.py
@@ -1,4 +1,4 @@
-# Import the libraries you will use throughout the code
+# STEP 0: Import the libraries you will use throughout the code
 import itertools
 import collections
 import tweepy
@@ -8,8 +8,21 @@ import sqlite3
 import re
 import datetime
 
-# Connect to tweepy and my Twitter account
-# NEVER put your keys/secrets directly in your source code! Always import from a separate file
+### IMPORTANT ###
+# You *MUST* run:
+# 1) ao3_work_ids.py to gather fic work IDs,
+
+# 2) ao3_get_fanfics.py to gather the fic data from the work IDs,
+
+# 3) extract_metadata.py to generate a CSV of only the metadata (i.e., no fanfic chapters since you don't need them),
+
+# 4) manually convert the metadata CSV to a JSON file (there is code for this, but it's easier for me using [https://www.convertcsv.com/csv-to-json.htm] - if you'd like to do it via code refer to [https://www.geeksforgeeks.org/convert-csv-to-json-using-python/] for steps), and
+
+# 5) generate_tags_list.py on the metadata JSON file to create a database and a list of tags to tweet BEFORE using this bot. There is no caching or cleaning of fic data in this code.
+
+
+# STEP 1: Connect to tweepy and your Twitter account (i.e., the tag account)
+# REMEMBER to NEVER put your keys/secrets directly in your source code! Always import from a separate file and do not push that file to GitHub.
 
 consumer_key = twitter_info_hannibal.consumer_key
 consumer_secret = twitter_info_hannibal.consumer_secret
@@ -20,73 +33,30 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
-# You will need to convert the ao3_get_fanfics.py CSV output file to JSON to write to the sqlite3 database. I did this using (https://www.convertcsv.com/csv-to-json.htm), however, there are ways to do this in your code. Refer to (https://www.geeksforgeeks.org/convert-csv-to-json-using-python/) for steps
 
-# Assign a variable to the JSON file you just converted
-CACHE_FNAME = "hannibal_fic_info2.json"
+# STEP 2: Open the file containing the final tags list.
 
-# Open the file so it can be used in the rest of the code
-try:
-	cache_file = open(CACHE_FNAME, 'r', encoding="cp1252")
-	cache_contents = cache_file.read()
-	cache_file.close()
-	CACHE_DICTION = json.loads(cache_contents)
-except:
-	print("cache not open")
+def read_file_extract_list(fname):
+	#'fname' is a file name with .txt at the end.
+	with open('fname', 'r') as file:
+		lines = file.read()
+		final_tags = lines.split('\n')
+		return final_tags
 
 
-# Write the initial information I want in the database
-# Connect to the sqlite3 by inputting a name for your database file
-conn = sqlite3.connect("hannibal_fics.db")
-cur = conn.cursor()
-
-# Creates a table if one does not exist
-cur.execute("DROP TABLE IF EXISTS HannibalFics")
-cur.execute("CREATE TABLE IF NOT EXISTS HannibalFics (work_id INTEGER PRIMARY KEY, title TEXT, relationship TEXT, additional_tags TEXT, link TEXT)")
-
-# For each dictionary in the list CACHE_DICTION, insert the relevant information into the database
-for fic in CACHE_DICTION:
-    cur.execute("INSERT INTO HannibalFics (work_id, title, relationship, additional_tags, link) VALUES (?, ?, ?, ?, ?)",
-                (fic['work_id'], fic['title'], fic['relationship'], fic['additional tags'], 'https://archiveofourown.org/works/' + str(fic['work_id'])))
-conn.commit()
-# conn.close()
-print("done with database!")
 
 
-###########################
-'''Have a function only run once (i.e., only run altar table once)
-
-I would use a decorator on the function to handle keeping track of how many times it runs.
-
-def run_once(f):
-    def wrapper(*args, **kwargs):
-        if not wrapper.has_run:
-            wrapper.has_run = True
-            return f(*args, **kwargs)
-    wrapper.has_run = False
-    return wrapper
 
 
-@run_once
-def my_function(foo, bar):
-    return foo+bar
-Now my_function will only run once. Other calls to it will return None. Just add an else clause to the if if you want it to return something else. From your example, it doesn't need to return anything ever.
 
-If you don't control the creation of the function, or the function needs to be used normally in other contexts, you can just apply the decorator manually as well.
 
-action = run_once(my_function)
-while 1:
-    if predicate:
-        action()
-This will leave my_function available for other uses.
 
-Finally, if you need to only run it once twice, then you can just do
 
-action = run_once(my_function)
-action() # run once the first time
 
-action.has_run = False
-action() # run once the second time
+
+
+
+
 
 
 practice = [
